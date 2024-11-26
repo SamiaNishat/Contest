b7674b08bc8aed1ab4e0dc54dfb00ef3e4ceee23
samia <samianishatox@gmail.com>
2024-11-26 19:31:46 +0600

    Accepted

--- Welcome To The World CP/A1.cpp
+++ Welcome To The World CP/A1.cpp
@@ -1,6 +1,15 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    
+int solveMeFirst(int a, int b)
+{
+    int sum = a + b;
+    return sum;
+}
+int main()
+{
+    int a, b, sum;
+    cin >> a >> b;
+    sum = solveMeFirst(a,b);
+    cout << sum;
 }
