3ff2cb8557fd28e51f01c92b76b4282875513de3
samia <samianishatox@gmail.com>
2024-11-26 20:54:36 +0600

    Accepted

--- Welcome To The World CP/B2.cpp
+++ Welcome To The World CP/B2.cpp
@@ -1,6 +1,25 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    
+int n;
+int simpleArraySum(int ar[])
+{
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        sum = sum + ar[i];
+    }
+    return sum;
+}
+
+int main()
+{
+    cin >> n;
+    int ar[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> ar[i];
+    }
+    int sum = simpleArraySum(ar);
+    cout << sum;
 }
