72066f1d30fbb4cdb8569b5ae9d144d047f655fa
Samia Nishat <168514687+samihanishat@users.noreply.github.com>
2025-05-06 11:32:30 +0600

    Cloudberry Jam
    Educational Codeforces Round 177 (Rated for Div. 2), problem: (A) Cloudberry Jam

---
+++ CFContest/Cloudberry Jam.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int berrie = (n * 3 * 2 / 3);
+        cout << berrie << endl;
+    }
+}
