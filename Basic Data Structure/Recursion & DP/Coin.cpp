e2a63befcca9fb1bbc62dabf410b687ae19038c7
Samia Nishat <168514687+SamiaNishat@users.noreply.github.com>
2025-11-19 10:51:00 +0600

    isPossible Coin without memoization O(m^n)

---
+++ Basic Data Structure/Recursion & DP/Coin.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> coins = {5,12,23};  // m = vector size / number of coin 
+int isPossible(int n){   //Time complexity  O(m^n)
+    if(n<0)return 0;
+    if(n==0)return 1;
+
+    for(int coin : coins){
+    	if(isPossible(n-coin)){
+    	 return 1;
+    	}
+    }
+
+    return 0;
+    
+}
+
+int main(){
+    int n;
+    cin>>n;
+    cout<<isPossible(n);
+}
