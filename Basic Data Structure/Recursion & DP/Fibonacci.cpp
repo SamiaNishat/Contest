ee73085ce94081406d992fcf26fa3f0043ca0c38
Samia Nishat <168514687+SamiaNishat@users.noreply.github.com>
2025-11-19 09:32:29 +0600

    Add Fibonacci function with memoization
    Implement Fibonacci function using memoization.

---
+++ Basic Data Structure/Recursion & DP/Fibonacci.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int a[10005];
+
+int fibb(int n){
+
+   if(n<=1)return n; // base case
+   
+   if(a[n]==-1)  // precalculation check
+     a[n] = fibb(n-1) + fibb(n-2);  // claculation & store
+  
+   return a[n];  // return
+}
+int main()
+{
+    int n;
+    cin>>n;
+    memset(a,-1,sizeof(a));
+    cout<<fibb(n)<<endl;
+}
